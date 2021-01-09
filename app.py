af42efbf5a0b8108f3f0014046bb06d83e13ca15
Xus72 <jexusth@hotmail.com>
2021-01-09 11:38:02 +0100

    method in app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, url_for,render_template
+from flask import Flask, url_for,render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods =["POST", "GET"])
+
 def index():
     return render_template("index.html", name="Index")
 
-#URL CSS
-#url_for('static', filename='style.css')
+
+if __name__ == '__main__':
+    app.run(debug=True)
